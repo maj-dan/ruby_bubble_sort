9ee698db4e64f37b22d480e6bff3c41419b51a61
maj-dan <160074910+maj-dan@users.noreply.github.com>
2025-04-24 22:38:53 -0300

    Write bubble_sort function and README

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ruby_bubble_sort
+
+**A simble bubble sort algorithm, writen in ruby**
+  
+Takes an array as argument
---
+++ bubble_sort.rb
@@ -0,0 +1,29 @@
+def bubble_sort(array)
+  last_index = array.length - 1
+  
+  loop do
+    has_swapped = false
+    #need to iterate array.length - 1 times because
+    #it's comparing pairs, else it will take an nil
+    #value somewhere
+    for index in 1..last_index do
+      previous_index = index - 1
+
+      if array[previous_index] > array[index]
+
+        #keep one value in a third varible
+        #can't assign two variables at same time
+        value_to_swap = array[index]
+        array[index] = array[previous_index]
+        array[previous_index] = value_to_swap
+        has_swapped = true
+      end
+    end
+    break unless has_swapped
+  end
+
+  array
+end
+
+p bubble_sort([4,3,78,2,0,2])
+p bubble_sort([4])
